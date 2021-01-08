9cdbaa91e213a6c0b2d0d21ff27e596c6d63bb4d
Dani <davillal@ucm.es>
2021-01-08 21:07:13 +0100

    Funcionando regular

---
+++ public/javascript/inicio.js
@@ -0,0 +1,57 @@
+"use strict"
+
+var rondas = 0;
+var aciertos = 0;
+var verdadera = 0;
+
+
+function iniciar(){
+    nuevaRonda();
+}
+
+function nuevaRonda(){
+    rondas++;
+    var imgR = Math.floor((Math.random() * 27) + 1);
+    var imgF = Math.floor((Math.random() * 30) + 1);
+
+    verdadera = Math.floor((Math.random() * 2) + 1);
+    var izquierda = document.createElement("IMG");
+    var derecha = document.createElement("IMG");     
+     
+
+    if(verdadera == 1){
+        izquierda.src = "../images/fotos/r" + imgR + ".jpg";
+        izquierda.style.width = "500px";
+        izquierda.style.height = "500px";
+
+        derecha.src = "../images/fotos/f" + imgF + ".jpg";
+        derecha.style.width = "500px";
+        derecha.style.height = "500px";
+    }
+
+    else{
+        izquierda.src = "../images/fotos/f" + imgF + ".jpg>";
+        izquierda.style.width = "500px";
+        izquierda.style.height = "500px";
+
+        derecha.src = "../images/fotos/r" + imgR + ".jpg";
+        derecha.style.width = "500px";
+        derecha.style.height = "500px";
+    }
+
+
+    document.getElementById('1').appendChild(izquierda);
+    document.getElementById('2').appendChild(derecha);
+}
+
+function comprobacion(id){
+    if(id == verdadera){
+        aciertos++;
+        document.getElementById("aciertos").innerHTML = "SI";
+    }
+    else{
+        document.getElementById("aciertos").innerHTML = "NO";
+    }
+
+    nuevaRonda();
+}
